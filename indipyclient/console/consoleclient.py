cd4b1a13f02e24cda77c4deadcb53d6049abcb63
Bernard Czenkusz <bernie@skipole.co.uk>
2023-08-22 00:29:05 +0100

    Working on client

--- indipyclient/console/consoleclient.py
+++ indipyclient/console/consoleclient.py
@@ -35,13 +35,16 @@ class ConsoleControl:
         curses.cbreak()
         self.origcursor = curses.curs_set(0)
         self.stdscr.keypad(True)
-        self.cols = curses.COLS
-        self.lines = curses.LINES
 
         # this keeps track of which screen is being displayed
         self.screen = "startscreen"
 
+        # this is set to True, to shut down the client
+        self._stop = False
+
+
     async def shutdown(self):
+        self._stop = True
         await self.client.shutdown()
         curses.nocbreak()
         self.stdscr.keypad(False)
@@ -49,28 +52,47 @@ class ConsoleControl:
         curses.echo()
         curses.endwin()
 
-    async def asyncrun(self):
-        try:
-            while True:
-                await asyncio.sleep(0)
-                try:
-                    event = self.eventque.get_nowait()
-                except asyncio.QueueEmpty:
-                    event = None
-                if not self.client.connected:
-                    # display the startscreen
-                    self.screen = "startscreen"
-                    messages = [ t.isoformat(sep='T')[11:21] + "  " + m for t,m in reversed(self.client.messages) ]
-                    widgets.startscreen(self.stdscr, "indipyclient console", "Not Connected", messages)
-                    await asyncio.sleep(2)
-                    continue
-                # to get here a connection must be made
-                if self.screen == "startscreen":
-                    messages = [ t.isoformat(sep='T')[11:21] + "  " + m for t,m in reversed(self.client.messages) ]
-                    widgets.startscreen(self.stdscr, "indipyclient console", "Connected", messages)
-                    await asyncio.sleep(2)
-                # some other screen etc....
+    async def showscreen(self):
+        while not self._stop:
+            await asyncio.sleep(0)
+            try:
+                event = self.eventque.get_nowait()
+            except asyncio.QueueEmpty:
+                event = None
+            if not self.client.connected:
+                # display the startscreen
+                self.screen = "startscreen"
+                messages = [ t.isoformat(sep='T')[11:21] + "  " + m for t,m in reversed(self.client.messages) ]
+                widgets.startscreen(self.stdscr, "indipyclient console", "Not Connected", messages)
+                await asyncio.sleep(2)
+                continue
+            # to get here a connection must be made
+            if self.screen == "startscreen":
+                messages = [ t.isoformat(sep='T')[11:21] + "  " + m for t,m in reversed(self.client.messages) ]
+                widgets.startscreen(self.stdscr, "indipyclient console", "Connected", messages)
+                await asyncio.sleep(2)
+            # some other screen etc....
+        # having brooken from while loop, shutdown the client
+        await self.shutdown()
 
-        except Exception:
-            await self.shutdown()
-            raise
+
+
+    async def getinput(self):
+        while not self._stop:
+            await asyncio.sleep(0)
+            if self.screen == "startscreen":
+                result = widgets.startinputs(self.stdscr)
+                if result == "quit":
+                    self._stop = True
+                    break
+                # if result == "Devices":
+                #     self.screen = "Devices"
+
+        # having brooken from while loop, shutdown the client
+        await self.shutdown()
+
+
+
+    async def asyncrun(self):
+        """Gathers tasks to be run simultaneously"""
+        await asyncio.gather(self.showscreen(), self.getinput(), return_exceptions=True)
--- indipyclient/console/widgets.py
+++ indipyclient/console/widgets.py
@@ -5,12 +5,20 @@ import curses
 def startscreen(stdscr, title, info, messages=[]):
     "Displays title, info string and list of messages on a start screen"
     stdscr.clear()
-    stdscr.addstr(0, 0, title, curses.A_REVERSE)
+    stdscr.addstr(0, 0, title, curses.A_BOLD)
     stdscr.addstr(2, 0, info)
     for count, m in enumerate(messages):
         if count:
             stdscr.addstr(4+count, 4, m)
         else:
             # highlight first message
-            stdscr.addstr(4+count, 4, m, curses.A_REVERSE)
+            stdscr.addstr(4+count, 4, m, curses.A_BOLD)
+    # put [Devices] [Quit] buttons on screen
+    stdscr.addstr( curses.LINES - 1, curses.COLS//2 - 10, "[Devices]", curses.A_REVERSE)
+    stdscr.addstr( curses.LINES - 1, curses.COLS//2 + 2, "[Quit]")
     stdscr.refresh()
+
+
+def startinputs(stdscr):
+    "Gets inputs from the screen"
+    pass
