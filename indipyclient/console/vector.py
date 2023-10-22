da7e9a5f8fad7cd6f428e050c8866327004cc10d
Bernard Czenkusz <bernie@skipole.co.uk>
2023-10-22 12:17:28 +0100

    Working on client

--- indipyclient/console/vector.py
+++ indipyclient/console/vector.py
@@ -25,16 +25,45 @@ class VectorScreen:
         self.devicename = devicename
         self.vectorname = vectorname
 
+        self.device = self.client[self.devicename]
+
         # title window  (2 lines, full row, starting at 0,0)
         self.titlewin = self.stdscr.subwin(2, self.maxcols, 0, 0)
         self.titlewin.addstr(0, 0, "Device: " + self.devicename, curses.A_BOLD)
         self.titlewin.addstr(1, 0, "Vector: " + self.vectorname, curses.A_BOLD)
 
+        # messages window (1 line, full row, starting at 3,0)
+        self.messwin = self.stdscr.subwin(1, self.maxcols, 3, 0)
+
+        # bottom buttons, [Devices] [Messages] [Quit]
+
+        # buttons window (1 line, full row, starting at  self.maxrows - 1, 0)
+        self.buttwin = self.stdscr.subwin(1, self.maxcols, self.maxrows - 1, 0)
+
+        self.devices_btn = widgets.Button(self.buttwin, "Devices", 0, self.maxcols//2 - 15)
+        self.devices_btn.focus = True
+        self.focus = "Devices"
+
+        self.messages_btn = widgets.Button(self.buttwin, "Messages", 0, self.maxcols//2 - 5)
+        self.quit_btn = widgets.Button(self.buttwin, "Quit", 0, self.maxcols//2 + 6)
+
 
     def show(self):
         "Displays the window"
 
+        if self.device.messages:
+            widgets.drawmessage(self.messwin, self.device.messages[0])
+
+        # draw the bottom buttons
+        self.devices_btn.draw()
+        self.messages_btn.draw()
+        self.quit_btn.draw()
+
+        #  and refresh
         self.titlewin.noutrefresh()
+        self.messwin.noutrefresh()
+        self.buttwin.noutrefresh()
+
         curses.doupdate()
         return
 
