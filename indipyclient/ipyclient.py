95851874c2213bf16a9a93f25a385195b603bae9
Bernard Czenkusz <bernie@skipole.co.uk>
2025-01-06 10:14:18 +0000

    version 0.6.7, set user string bugfix, and small doc change

--- indipyclient/ipyclient.py
+++ indipyclient/ipyclient.py
@@ -769,9 +769,18 @@ Setting it to None will transmit an enableBLOB for all devices set to the enable
 
     async def send_newVector(self, devicename, vectorname, timestamp=None, members={}):
         """Send a Vector with updated member values, members is a membername
-           to value dictionary. Note, if this vector is a BLOB Vector, the members
-           dictionary should be {membername:(value, blobsize, blobformat)}
-           where value could be a bytes object or a filepath"""
+           to value dictionary.
+
+           Note, if this vector is a BLOB Vector, the members dictionary should be
+           {membername:(value, blobsize, blobformat)}
+           where value could be a bytes object, a pathlib.Path, or a string filepath
+           If blobsize of zero is used, the size value sent will be set to the number of bytes
+           in the BLOB. The INDI standard specifies the size should be that of the BLOB
+           before any compression, therefore if you are sending a compressed file, you
+           should set the blobsize prior to compression.
+           blobformat should be a file extension, such as '.png'. If it is an empty string
+           and value is a filename, the extension will be taken from the filename."""
+
         device = self.data.get(devicename)
         if device is None:
             return
